7236948c12fc92e33408a308b96bf981157c8cda
anthonynguyen92 <thang@DESKTOP-CLIGRLC.(none)>
2020-03-21 16:52:38 +0700

    add api and update database

---
+++ Motel.Application/Category/BillPayment/IManageBillPayment.cs
@@ -0,0 +1,18 @@
+﻿using Motel.Application.Category.BillPayment.Dtos;
+using Motel.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motel.Application.Category.BillPayment
+{
+    public interface IManageBillPayment
+    {
+        Task<int> Create(BillPaymentCreate create);
+        Task<int> Update(BillPaymentUpdate update);
+        Task<int> Delete(BillPaymentDelete delete);
+        Task<List<BillPaymentViewModel>> GetAll();
+        Task<PagedViewModel<BillPaymentViewModel>> GetAllPaging(int pIndex, int pSize);
+    }
+}
