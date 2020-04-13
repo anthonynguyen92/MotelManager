c9a364454192a4bfa61438d10e83d8ba13e4703d
anthonynguyen92 <thang@DESKTOP-CLIGRLC.(none)>
2020-04-13 08:39:58 +0700

    add exception

---
+++ Motel.Utilities/Helper/SelfHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motel.Utilities.Helper
+{
+    public class SelfHttpClient : ISelfHttpClient
+    {
+        private readonly HttpClient _client;
+        public SelfHttpClient(HttpClient client , IHttpContextAccessor httpContextAccessor) 
+        {
+            string baseString = 
+        }
+        public Task PostIdAsync(string apiRoute, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
